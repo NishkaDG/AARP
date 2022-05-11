bf9b2807731eb036fd6d35dccc75a70f20410f20
Rasmus Thorsøe <rt071195@gmail.com>
2022-05-11 12:43:28 +0200

    ghostwriting

---
+++ test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppReversePInvokeWrapperTable.cpp
@@ -0,0 +1,197 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <stdint.h>
+#include <limits>
+
+
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915;
+
+struct Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2;
+struct ovrAvatar2Asset_Resource_tD4EE2260ED53225900F2FFC036DBFB95214949D7;
+struct ovrAvatar2HandTrackingState_t7E02FA3E73826E5B67E1CD266E32ED481DA74E62;
+struct ovrAvatar2InputControlState_t38BFA93F0C206B484597BA03072D97CA940EAC91;
+struct ovrAvatar2InputTrackingState_tAC1C5A06D576A4DDE3A7B72C07678EFFB4FB5A7B;
+struct ovrAvatar2LipSyncState_t79040F0F16B27BD80CDFDDDE37AA1957DA7B0565;
+struct ovrAvatar2TrackingBodyPose_t76E92A258678C554C31999A7E234477F88AD9E61;
+struct ovrAvatar2TrackingBodySkeleton_tE76214C4AE6A491CA80596B3B987F77E44376745;
+struct ovrAvatar2TrackingBodyState_tDA80D8FD01FB597A32B337B4917446B1C8161D93;
+struct unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902;
+struct unitytls_key_ref_t6BD91D013DF11047C53738FEEB12CE290FDC71A2;
+struct unitytls_tlsctx_tF8BBCBFE1E957B846442DED65ECB89BC5307DEAE;
+struct unitytls_x509list_ref_t6C5C1CF0B720516A681CB741104A164FD8B3CF17;
+struct unitytls_x509name_t8A1108C917795D8FE946B50769ACE51489C7BF5D;
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+
+// System.IntPtr
+struct IntPtr_t 
+{
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+};
+
+struct IntPtr_t_StaticFields
+{
+	// System.IntPtr System.IntPtr::Zero
+	intptr_t ___Zero_1;
+};
+
+// System.UIntPtr
+struct UIntPtr_t 
+{
+	// System.Void* System.UIntPtr::_pointer
+	void* ____pointer_1;
+};
+
+struct UIntPtr_t_StaticFields
+{
+	// System.UIntPtr System.UIntPtr::Zero
+	uintptr_t ___Zero_0;
+};
+
+// UnityEngine.Vector3
+struct Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 
+{
+	// System.Single UnityEngine.Vector3::x
+	float ___x_2;
+	// System.Single UnityEngine.Vector3::y
+	float ___y_3;
+	// System.Single UnityEngine.Vector3::z
+	float ___z_4;
+};
+
+struct Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2_StaticFields
+{
+	// UnityEngine.Vector3 UnityEngine.Vector3::zeroVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___zeroVector_5;
+	// UnityEngine.Vector3 UnityEngine.Vector3::oneVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___oneVector_6;
+	// UnityEngine.Vector3 UnityEngine.Vector3::upVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___upVector_7;
+	// UnityEngine.Vector3 UnityEngine.Vector3::downVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___downVector_8;
+	// UnityEngine.Vector3 UnityEngine.Vector3::leftVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___leftVector_9;
+	// UnityEngine.Vector3 UnityEngine.Vector3::rightVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___rightVector_10;
+	// UnityEngine.Vector3 UnityEngine.Vector3::forwardVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___forwardVector_11;
+	// UnityEngine.Vector3 UnityEngine.Vector3::backVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___backVector_12;
+	// UnityEngine.Vector3 UnityEngine.Vector3::positiveInfinityVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___positiveInfinityVector_13;
+	// UnityEngine.Vector3 UnityEngine.Vector3::negativeInfinityVector
+	Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___negativeInfinityVector_14;
+};
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+
+// Mono.Unity.UnityTls/unitytls_x509_ref
+struct unitytls_x509_ref_t9CEB17766B4144117333AB50379B21A357FA4333 
+{
+	// System.UInt64 Mono.Unity.UnityTls/unitytls_x509_ref::handle
+	uint64_t ___handle_0;
+};
+
+// Mono.Unity.UnityTls/unitytls_x509list_ref
+struct unitytls_x509list_ref_t6C5C1CF0B720516A681CB741104A164FD8B3CF17 
+{
+	// System.UInt64 Mono.Unity.UnityTls/unitytls_x509list_ref::handle
+	uint64_t ___handle_0;
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_CAPI_LoggingCallback_mC8F1FFFEB41AD7FD789867B76F9F887A8CED44D2(intptr_t ___str0);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_CultureInfo_OnCultureInfoChangedInAppX_mDBD419B094B2CFE933BB3F63886A5AB4E44D2DC0(Il2CppChar* ___language0);
+extern "C" int32_t CDECL ReversePInvokeWrapper_DeflateStreamNative_UnmanagedRead_m79E9628F6FF91A3995491997242DD83046C9B704(intptr_t ___buffer0, int32_t ___length1, intptr_t ___data2);
+extern "C" int32_t CDECL ReversePInvokeWrapper_DeflateStreamNative_UnmanagedWrite_m509AE153ECB916CDF755432335FBAB2B51CA1486(intptr_t ___buffer0, int32_t ___length1, intptr_t ___data2);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_mB85BF0265E239960FC963DCA74DC67EBCE9480AC(intptr_t ___arg0);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_OculusSpatializerUnity_AudioRaycast_m04A1F287277FD023D5B43961652989F43B38DF83(Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___origin0, Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2 ___direction1, Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2* ___point2, Vector3_t24C512C7B96BBABAD472002D0BA2BDA40A5A80B2* ___normal3, intptr_t ___data4);
+extern "C" int32_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_HandTrackingCallback_m91C8329CCAA1F7DBFF8338E11323350D11346457(ovrAvatar2HandTrackingState_t7E02FA3E73826E5B67E1CD266E32ED481DA74E62* ___handsState0, intptr_t ___context1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_InputControlCallback_m771FDE2F342DDA8421ADA08F027D8B2126D14026(ovrAvatar2InputControlState_t38BFA93F0C206B484597BA03072D97CA940EAC91* ___controlState0, intptr_t ___userContext1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_InputTrackingCallback_m030CF3358FFB8E860B9B459A2F8D6EC74658A815(ovrAvatar2InputTrackingState_tAC1C5A06D576A4DDE3A7B72C07678EFFB4FB5A7B* ___trackingState0, intptr_t ___userContext1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodyPoseCallback_mB7630C37E82B3DA83C6BDCCFD2F84598E82F30FB(ovrAvatar2TrackingBodyPose_t76E92A258678C554C31999A7E234477F88AD9E61* ___nativePose0, intptr_t ___userContext1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodySkeletonCallback_m358EEC98A96C2BBD4255F915C54941425C2D0335(ovrAvatar2TrackingBodySkeleton_tE76214C4AE6A491CA80596B3B987F77E44376745* ___nativeSkeleton0, intptr_t ___userContext1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodyStateCallback_mD0E08012FF45CC9E999A81F0B6100DAC34DE43E0(ovrAvatar2TrackingBodyState_tDA80D8FD01FB597A32B337B4917446B1C8161D93* ___bodyState0, intptr_t ___userContext1);
+extern "C" uint8_t CDECL ReversePInvokeWrapper_OvrAvatarInputTrackingContextBase_InputTrackingCallback_mF5EF0FE795B616A5F9CB405BE8EADFBD391C3134(ovrAvatar2InputTrackingState_tAC1C5A06D576A4DDE3A7B72C07678EFFB4FB5A7B* ___inputTrackingState0, intptr_t ___userContext1);
+extern "C" int32_t CDECL ReversePInvokeWrapper_OvrAvatarLipSyncContextBase_LipSyncCallback_mA31EA8EC607EF16FF5AF99DBAC4288FD87BE893A(ovrAvatar2LipSyncState_t79040F0F16B27BD80CDFDDDE37AA1957DA7B0565* ___lipsyncState0, intptr_t ___userContext1);
+extern "C" void CDECL ReversePInvokeWrapper_OvrAvatarLog_LogCallBack_m8E4D856D2F1D3BB664D706465F2F992D007B410A(int32_t ___priority0, char* ___msg1, intptr_t ___context2);
+extern "C" void CDECL ReversePInvokeWrapper_OvrAvatarManager_RequestCallback_mB17344FACCD53C50E1001D20FCE9DFFA2A1BF9FC(int32_t ___requestId0, int32_t ___result1, intptr_t ___userContext2);
+extern "C" void CDECL ReversePInvokeWrapper_OvrAvatarManager_ResourceCallback_m015BAA238EB383C519905459B5F9BBDA4B888958(ovrAvatar2Asset_Resource_tD4EE2260ED53225900F2FFC036DBFB95214949D7* ___resource0, intptr_t ___context1);
+extern "C" void CDECL ReversePInvokeWrapper_SocialPlatformManager_MicFilter_m072AADB806891708D9084215A181052D5E5B32A1(int16_t* ___pcmData0, uintptr_t ___pcmDataLength1, int32_t ___frequency2, int32_t ___numChannels3);
+extern "C" void CDECL ReversePInvokeWrapper_UnityTlsContext_CertificateCallback_mF5E626BA2545CFFA64428622678E409702C2045A(void* ___userData0, unitytls_tlsctx_tF8BBCBFE1E957B846442DED65ECB89BC5307DEAE* ___ctx1, uint8_t* ___cn2, intptr_t ___cnLen3, unitytls_x509name_t8A1108C917795D8FE946B50769ACE51489C7BF5D* ___caList4, intptr_t ___caListLen5, unitytls_x509list_ref_t6C5C1CF0B720516A681CB741104A164FD8B3CF17* ___chain6, unitytls_key_ref_t6BD91D013DF11047C53738FEEB12CE290FDC71A2* ___key7, unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902* ___errorState8);
+extern "C" intptr_t CDECL ReversePInvokeWrapper_UnityTlsContext_ReadCallback_m15F3A217E44D480CAED06EB5A88503FB6259D7EC(void* ___userData0, uint8_t* ___buffer1, intptr_t ___bufferLen2, unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902* ___errorState3);
+extern "C" uint32_t CDECL ReversePInvokeWrapper_UnityTlsContext_VerifyCallback_m75D7C072718405EBBF8A2A9C794C4DDFB2595BD0(void* ___userData0, unitytls_x509list_ref_t6C5C1CF0B720516A681CB741104A164FD8B3CF17 ___chain1, unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902* ___errorState2);
+extern "C" intptr_t CDECL ReversePInvokeWrapper_UnityTlsContext_WriteCallback_m53DD70115C97432A676F3E437E8FB42F9FC068F5(void* ___userData0, uint8_t* ___data1, intptr_t ___bufferLen2, unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902* ___errorState3);
+extern "C" uint32_t CDECL ReversePInvokeWrapper_UnityTlsProvider_x509verify_callback_m47823254E133F7304ABFAE5A1F0D218402BC5B45(void* ___userData0, unitytls_x509_ref_t9CEB17766B4144117333AB50379B21A357FA4333 ___cert1, uint32_t ___result2, unitytls_errorstate_tC926EE4582920BE2C1DB1F3F65619B810D5AB902* ___errorState3);
+
+
+IL2CPP_EXTERN_C const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[];
+const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[23] = 
+{
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_CAPI_LoggingCallback_mC8F1FFFEB41AD7FD789867B76F9F887A8CED44D2),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_CultureInfo_OnCultureInfoChangedInAppX_mDBD419B094B2CFE933BB3F63886A5AB4E44D2DC0),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_DeflateStreamNative_UnmanagedRead_m79E9628F6FF91A3995491997242DD83046C9B704),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_DeflateStreamNative_UnmanagedWrite_m509AE153ECB916CDF755432335FBAB2B51CA1486),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_mB85BF0265E239960FC963DCA74DC67EBCE9480AC),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OculusSpatializerUnity_AudioRaycast_m04A1F287277FD023D5B43961652989F43B38DF83),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_HandTrackingCallback_m91C8329CCAA1F7DBFF8338E11323350D11346457),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_InputControlCallback_m771FDE2F342DDA8421ADA08F027D8B2126D14026),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContext_InputTrackingCallback_m030CF3358FFB8E860B9B459A2F8D6EC74658A815),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodyPoseCallback_mB7630C37E82B3DA83C6BDCCFD2F84598E82F30FB),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodySkeletonCallback_m358EEC98A96C2BBD4255F915C54941425C2D0335),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarBodyTrackingContextBase_BodyStateCallback_mD0E08012FF45CC9E999A81F0B6100DAC34DE43E0),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarInputTrackingContextBase_InputTrackingCallback_mF5EF0FE795B616A5F9CB405BE8EADFBD391C3134),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarLipSyncContextBase_LipSyncCallback_mA31EA8EC607EF16FF5AF99DBAC4288FD87BE893A),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarLog_LogCallBack_m8E4D856D2F1D3BB664D706465F2F992D007B410A),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarManager_RequestCallback_mB17344FACCD53C50E1001D20FCE9DFFA2A1BF9FC),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OvrAvatarManager_ResourceCallback_m015BAA238EB383C519905459B5F9BBDA4B888958),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_SocialPlatformManager_MicFilter_m072AADB806891708D9084215A181052D5E5B32A1),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_UnityTlsContext_CertificateCallback_mF5E626BA2545CFFA64428622678E409702C2045A),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_UnityTlsContext_ReadCallback_m15F3A217E44D480CAED06EB5A88503FB6259D7EC),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_UnityTlsContext_VerifyCallback_m75D7C072718405EBBF8A2A9C794C4DDFB2595BD0),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_UnityTlsContext_WriteCallback_m53DD70115C97432A676F3E437E8FB42F9FC068F5),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_UnityTlsProvider_x509verify_callback_m47823254E133F7304ABFAE5A1F0D218402BC5B45),
+};
